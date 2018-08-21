542f17796d2da0edae0823e4bf9b3314c3b48b27
Eduardo Fernandes <edu.fernandes.pt@gmail.com>
2018-08-21 20:35:39 +0100

    Implement the Url entity

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-urlShortener
-============
+# urlShortener
 
-A Symfony project created on August 21, 2018, 8:46 pm.
+Create the database
+
+```sql
+CREATE DATABASE url_sortener CHARACTER SET utf8 COLLATE utf8_genral_ci;
+CREATE USER 'url_admin'@'localhost' IDENTIFIED BY '12345678';
+GRANT ALL PRIVILEGES ON url_sortener.* TO 'url_admin'@'localhost';
+FLUSH PRIVILEGES;
+```
+
+Fill the data in the `app/config/parameters.yml` file:
+
+```yaml
+parameters:
+    database_host: 127.0.0.1
+    database_port: null
+    database_name: url_sortener
+    database_user: url_admin
+    database_password: 12345678
+```
---
+++ src/AppBundle/Entity/Url.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Url.
+ *
+ * @ORM\Table(name="url")
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\Repository\UrlRepository")
+ */
+class Url
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="hash", type="string", length=255, unique=true)
+     */
+    private $hash;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=2083, unique=true)
+     */
+    private $url;
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set hash.
+     *
+     * @param string $hash
+     *
+     * @return Url
+     */
+    public function setHash($hash)
+    {
+        $this->hash = $hash;
+
+        return $this;
+    }
+
+    /**
+     * Get hash.
+     *
+     * @return string
+     */
+    public function getHash()
+    {
+        return $this->hash;
+    }
+
+    /**
+     * Set url.
+     *
+     * @param string $url
+     *
+     * @return Url
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url.
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+}
